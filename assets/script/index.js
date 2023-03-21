8839ce803b05a09b87e3e92b5f203398cc063b0b
tamanchichan <tamanchichan@gmail.com>
2023-03-21 10:40:52 -0500

    post-box created

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const inputFile = document.querySelector('input[type=file]');
+const output = document.querySelector('.file-name');
+
+inputFile.addEventListener('change', () => {
+  const file = inputFile.files[0];
+  const fileName = file.name;
+  output.innerText = fileName;
+})
