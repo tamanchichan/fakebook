43bd48108796c53eb30b9a2a0fedc0a58818266c
tamanchichan <tamanchichan@gmail.com>
2023-03-21 23:15:33 -0500

    date added

--- assets/script/index.js
+++ assets/script/index.js
@@ -24,68 +24,6 @@ const textarea = document.querySelector('textarea');
 const button = document.querySelector('button');
 const content = document.querySelector('.content');
 
-  // button.addEventListener('click', () => {
-  // function createPost() {
-  //   const div = document.createElement('div');
-  //   const div1 = document.createElement('div');
-  //   const div2 = document.createElement('div');
-  //   const div3 = document.createElement('div');
-  //   const div4 = document.createElement('div');
-  //   const div5 = document.createElement('div');
-  //   const div6 = document.createElement('div');
-  //   const div7 = document.createElement('div');
-  //   const div8 = document.createElement('div');
-  //   const div9 = document.createElement('div');
-    
-  //   content.appendChild(div);
-  //   div.classList.add('post');
-    
-  //   const post = document.querySelector('.post');
-  //   post.appendChild(div1);
-  //   div1.classList.add('post-header', 'flex');
-    
-  //   post.appendChild(div2);
-  //   div2.classList.add('post-main');
-    
-  //   const postHeader = document.querySelector('.post-header');
-  //   postHeader.appendChild(div3);
-  //   div3.classList.add('post-header-column-1')
-  //   postHeader.appendChild(div4);
-  //   div4.classList.add('post-header-column-2');
-    
-  //   const postHeaderColumn1 = document.querySelector('.post-header-column-1');
-  //   postHeaderColumn1.appendChild(div5);
-  //   div5.classList.add('user');
-  //   postHeaderColumn1.appendChild(div6);
-  //   div6.classList.add('username');
-    
-  //   const postHeaderColumn2 = document.querySelector('.post-header-column-2');
-  //   postHeaderColumn2.appendChild(div7);
-  //   div7.classList.add('date');
-    
-  //   const postMain = document.querySelector('.post-main');
-  //   postMain.appendChild(div8);
-  //   div8.classList.add('text');
-  //   postMain.appendChild(div9);
-  //   div9.classList.add('img');
-    
-  // inputFile.addEventListener('change', () => {
-  //   const file = inputFile.files[0];
-  //   const reader = new FileReader();
-  //   reader.addEventListener('load', () => {
-  //     const image = new Image();
-  //     image.src = reader.result;
-  //     div9.appendChild(image);
-  //   });
-  //   reader.readAsDataURL(file);
-  // });
-  
-  // div8.innerHTML = text.value;
-  // div9.innerHTML = inputFile.value;
-    
-  //   content.insertBefore(div, content.firstChild);
-  // };
-
 // button.addEventListener('click', () => {  
 //   function createPost() {
 //     const post = document.createElement('div');
@@ -142,15 +80,20 @@ button.addEventListener('click', () => {
     const text = document.createElement('div');
     const image = document.createElement('div');
     
+    
+    
     post.classList.add('post');
-    postHeader.classList.add('post-header');
+    postHeader.classList.add('post-header', 'flex');
     postHeaderColumn1.classList.add('post-header-column-1');
-    postHeaderColumn2.classList.add('post-header-column-2');
     user.classList.add('user');
     username.classList.add('username');
+    postHeaderColumn2.classList.add('post-header-column-2');
+    date.classList.add('date');
     postMain.classList.add('post-main');
     text.classList.add('text');
     image.classList.add('img');
+    
+    date.innerHTML = new Date().toDateString().slice(4);
     
     content.appendChild(post);
     post.appendChild(postHeader);
